c0be8644412440880c64719850a6d528f1f7cddc
Scott-Canning <34758484+Scott-Canning@users.noreply.github.com>
2022-03-09 10:39:14 -0500

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,26 @@
+const hre = require("hardhat");
+const { ethers } = require('ethers');
+require('dotenv').config();
+
+
+async function deploy() {
+  const url = process.env.KOVAN_URL;
+  const provider = new ethers.providers.JsonRpcProvider(url);
+
+  let privateKey = process.env.PRIVATE_KEY;
+  let wallet = new ethers.Wallet(privateKey, provider);
+
+  let artifacts = await hre.artifacts.readArtifact("DepositWithdraw");
+  let factory = new ethers.ContractFactory(artifacts.abi, artifacts.bytecode, wallet);
+  let contract = await factory.deploy();
+
+  console.log("Contract address:", contract.address);
+  await contract.deployed();
+}
+
+deploy()
+.then(() => process.exit(0))
+.catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
